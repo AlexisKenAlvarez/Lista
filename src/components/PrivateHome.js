9a3ba34be6d3e5dd153e322573f6c3124a8771eb
Alexis Ken Alvarez <alexisken1432@gmail.com>
2022-11-20 22:00:56 +0800

    Debug

--- src/components/PrivateHome.js
+++ src/components/PrivateHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AnimatePresence } from 'framer-motion'
 import { useNavigate } from 'react-router-dom'
 import Axios from 'axios'
@@ -19,41 +19,47 @@ export const PrivateHome = () => {
     const [finished, setFinished] = useState(0)
     const [active, setActive] = useState(0)
 
-    Axios.get(`${process.env.REACT_APP_BASEURL}/login`).then((response) => {
-        console.log("TEST LOGIN")
-        console.log(response)
-        if (response.data?.loggedIn) {
+    useEffect(() => {
+        Axios.get(`${process.env.REACT_APP_BASEURL}/login`).then((response) => {
+            console.log("TEST LOGIN")
+            console.log(response)
+            if (response.data?.loggedIn) {
+    
+                Axios.get(`${process.env.REACT_APP_BASEURL}/tasks`).then((response) => {
+                    const data = response.data.userData
+                    const active = response.data.userData.activeTask
+                    const finished = response.data.userData.finishedTask
+                    setActive(data.activeTask.length)
+                    setFinished(data.finishedTask.length)
+    
+                    // PUT LIST OF DATA INTO REDUX STATE
+                    if (finished.length > 0) {
+                        dispatch(setFinished({ value: response.data.userData.finishedTask }))
+                    }
+    
+                    if (active.length > 0) {
+                        dispatch(setList({ value: response.data.userData.activeTask }))
+                    }
+              
+                    
+    
+    
+                    setLog(true)
+    
+                  })
+    
+            } else {
+                setLog(false)
+    
+                navigate("/", { replace: true })
+    
+            }
+        })
+    
+    }, [])
+    
 
-            Axios.get(`${process.env.REACT_APP_BASEURL}/tasks`).then((response) => {
-                const data = response.data.userData
-                const active = response.data.userData.activeTask
-                const finished = response.data.userData.finishedTask
-                setActive(data.activeTask.length)
-                setFinished(data.finishedTask.length)
-
-                // PUT LIST OF DATA INTO REDUX STATE
-                if (finished.length > 0) {
-                    dispatch(setFinished({ value: response.data.userData.finishedTask }))
-                }
-
-                if (active.length > 0) {
-                    dispatch(setList({ value: response.data.userData.activeTask }))
-                }
-          
-                
-
-
-                setLog(true)
-
-              })
-
-        } else {
-            setLog(false)
-
-            navigate("/", { replace: true })
-
-        }
-    })
+    
 
     if (!loggedIn) {
         return (
